7c2291903c1da9db904b835edfa152d1480b0480
OR-2000 <71973242+OR-2000@users.noreply.github.com>
2020-12-10 12:17:08 +0900

    feat: add component for show timetable

---
+++ src/components/TimeTable.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>hello</div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+
+  name: 'TimeTable',
+
+  data: () => ({
+  }),
+
+});
+</script>
