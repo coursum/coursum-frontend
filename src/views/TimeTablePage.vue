dc1007ef8c7172ff6235ebd39cd9166d30f97e21
OR-2000 <71973242+OR-2000@users.noreply.github.com>
2020-12-27 14:42:04 +0900

    feat: add timetable

--- src/views/TimeTablePage.vue
+++ src/views/TimeTablePage.vue
@@ -1,169 +1,106 @@
 <template>
-  <div v-if="!isLoading">
-    <time-table />
-    <!-- <div class="d-flex align-center flex-column py-12">
-      <div>TimeTable</div>
-      <v-dialog
-        v-model="dialog2"
-        persistent
-        max-width="500"
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <span>
-            <v-btn
-              style="min-width: 0"
-              outlined
-              v-bind="attrs"
-              small
-              v-on="on"
-              @click="
-                dialog2 = true;
-                convertToURL();
-              "
-            >
-              <v-icon small>mdi-share</v-icon>
-            </v-btn>
-          </span>
-        </template>
-        <v-card class="pa-6">
-          <v-card-text>以下のリンクで、時間割を共有できます</v-card-text>
-          <v-text-field
-            id="input"
-            v-model="convertedURL"
-            :prepend-icon="'mdi-clipboard-text-multiple-outline'"
-            readonly
-            @click:prepend="doCopy"
-          />
-
-          <v-card-actions>
-            <v-chip
-              v-if="copiedURL"
-              class="ma-2"
-              color="teal"
-              text-color="white"
-            >
-              <v-avatar left>
-                <v-icon>mdi-checkbox-marked-circle</v-icon>
-              </v-avatar>
-              Copied!
-            </v-chip>
-            <v-spacer />
-            <v-btn
-              color="green darken-1"
-              text
-              @click="closeDialog"
-            >
-              完了
-            </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
+  <div class="pa-6">
+    <div v-if="isLoading">
+      now loading...
     </div>
-
-    <div
-      v-for="(dayArray, idx) in tableDatas"
-      :key="idx"
-      class="ma-2 py-2"
-    >
+    <div v-else>
       <div
-        style="color: #929292"
-        class="text-center"
+        v-for="(d, i) in sorted"
+        :key="i"
       >
-        {{ $t("days")[idx] }}
-      </div>
-
-      <v-divider />
-
-      <div class="py-3">
-        <div
-          v-for="(courseIdList, index) in dayArray"
-          :key="index"
-        >
+        <div class="my-6">
           <div
-            v-if="courseIdList.some((e) => e !== -1)"
-            class="d-flex justify-center my-1"
+            v-if="idx(d) === 100"
+            class="secondary--text font-weight-bold"
           >
-            <v-btn
-              v-if="courseIdList.length > 1"
-              style="min-width: 0; color: #929292"
-              text
-            >
-              {{ index + 1 }}
-            </v-btn>
-
-            <v-card
-              v-if="courseIdList.length > 1"
-              elevation="2"
-              tile
-              style="width: 100%"
-              class="px-2"
-            >
-              <div
-                v-for="(courseId, i) in courseIdList.slice(1)"
-                :key="i"
-                class="d-flex justify-space-between align-center"
-              >
-                <div
-                  v-if="
-                    courseData.title && courseData.title.name
-                      && courseData.title.name[`${curLang()}`]
-                  "
-                >
-                  <span
-
-                    class="text-caption"
-                  >{{
-                    coursesInTimeTable[courseId].title.name[`${curLang()}`]
-                  }}</span>
-
-                  <span class="d-flex flex-row">
-                    <router-link
-                      :to="'/course-detail/' + courseData.title.name.en"
-                      class="text-decoration-none"
-                    >
-                      <v-btn icon>
-                        <v-icon small>
-                          mdi-file-document-outline
-                        </v-icon>
-                      </v-btn>
-                    </router-link>
-
-                  </span>
-                </div>
-              </div>
-            </v-card>
+            {{ $t("days")[6] }}
           </div>
+          <div
+            v-else
+            class="secondary--text font-weight-bold"
+          >
+            {{ $t("days")[idx(d)] }}
+          </div>
+
+          <course-data
+            v-for="(t, n) in d"
+            :key="n"
+            :course-data="courseDatas[t[0]]"
+            :show-summary="false"
+          />
         </div>
       </div>
-    </div> -->
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import TimeTable from '@/components/TimeTable.vue';
+import axios from 'axios';
+import { CourseInfo } from '@/assets/CourseInfo';
+import CourseData from '@/components/CourseData.vue';
 
 export default Vue.extend({
 
   name: 'TimeTablePage',
+
   components: {
-    TimeTable,
+    CourseData,
   },
 
   data() {
     return {
-      dialog: false,
-      dialog2: false,
-      passphrase: '',
-      convertedURL: '',
-      tableDatas: [...Array(6).fill(0).map(() => Array(7).fill(0).map(() => [-1])), [[-1]]],
-      days: ['月', '火', '水', '木', '金', '土', 'その他'],
-      copiedURL: false,
       isLoading: true,
       isSharedPage: false,
+      courseDatas: {} as {[key: string]: CourseInfo},
+      sortedIDs: [] as [string, number[]][],
     };
   },
   computed: {
+    idsInTimetable(): string[] {
+      return this.$store.state.idsInTimetable;
+    },
+    sorted(): [string, number[]][][] {
+      const daySort = this.sortedIDs;
+      const result: [string, number[]][][] = [];
+      let temp: [string, number[]][] = [];
+
+      daySort.sort(([, [day1]], [, [day2]]) => day1 - day2);
+
+      let yesterday = 0;
+
+      daySort.forEach((arr, idx) => {
+        const [, [curDay]] = arr;
+
+        if (idx === daySort.length - 1) {
+          result.push([...temp, arr]);
+        } else if (yesterday === curDay) {
+          temp.push(arr);
+        } else {
+          result.push(temp);
+          temp = [arr];
+          yesterday = curDay;
+        }
+      });
+
+      // result.forEach((arr) => {
+      //   arr.sort(([, [, time1]], [, [, time2]]) => time1 - time2);
+      // });
+      return result;
+    },
+  },
+
+  async created() {
+    this.isLoading = true;
+    try {
+      this.idsInTimetable.forEach((str: string) => {
+        this.fetchData(str);
+      });
+    } catch (e) {
+      this.$router.push('/404');
+    } finally {
+      this.isLoading = false;
+    }
   },
   mounted() {
     if (this.$route.path.split('/')[2] === 'shared') {
@@ -173,60 +110,55 @@ export default Vue.extend({
     }
   },
   methods: {
-    idsInTimetable(): string[] {
-      return this.$store.state.idsInTimetable;
+    idx(d: [string, number[]][]): number {
+      return d?.[0]?.[1]?.[0];
     },
-    updateTable() {
-      // const tempTableDatas = [...Array(6).fill(0)
-      //   .map(() => Array(7)
-      //     .fill(0).map(() => [-1])), [[-1]]];
-      // let idsList = [];
-      // if (this.isSharedPage) {
-      //   const arr = this.$route.path.split('/')[3];
-      //   if (arr) {
-      //     idsList = arr.split('_');
-      //   }
-      // } else {
-      //   idsList = this.$store.state.idsInTimetable.slice(1);
-      // }
-      // this.coursesInTimeTable = idsList
-      //   .map((id: string | null) => this.courseDatas
-      //     .filter((data) => data.title.name.en === id));
-      // this.coursesInTimeTable.forEach((courseInTimeTable: CourseInfo, index: number) => {
-      //   if (courseInTimeTable.schedule === undefined
-      //       || courseInTimeTable.schedule.times === undefined
-      //     || courseInTimeTable.schedule.times.en === null) {
-      //     tempTableDatas[6][0].push(index); // put in "others"
-      //   } else {
-      //     const classInfo = courseInTimeTable.schedule.times.en[0].split(' ');
-      //     const classTime: number = parseInt(classInfo[1][0], 10);
-      //     const classDay: number = ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
-      // 'Friday', 'Saturday']
-      //       .findIndex((day) => day === classInfo[0]);
-      //     tempTableDatas[classDay][classTime - 1].push(index);
-      //   }
-      // });
-      // this.tableDatas = tempTableDatas;
-    },
+    async fetchData(str: string) {
+      let datas;
+      const url = new URL('http://54.248.214.173:8000/search');
+      url.search = `query=${str}`;
 
-    convertToURL() {
-      this.convertedURL = `http://localhost:8080/timetable/shared/${this.idsInTimetable().slice(1).join('_')}`;
-      this.passphrase = '';
-    },
-    doCopy() {
-      const copyText = document.querySelector('#input') as HTMLInputElement;
-      if (copyText !== null) {
-        copyText.select();
+      try {
+        const response = await axios.get(url.href);
+        datas = JSON.parse(JSON.stringify(response.data)).Hits;
+      } catch (e) {
+        this.$router.push('/404');
+      } finally {
+        this.setCourseDatas(datas);
       }
-      document.execCommand('copy');
-      this.copiedURL = true;
     },
-    closeDialog() {
-      this.dialog2 = false;
-      this.copiedURL = false;
+    setCourseDatas(datas: CourseInfo[]) {
+      datas
+        .some((dataObj: CourseInfo) => {
+          const id = `${dataObj?.title?.name?.jp} ${dataObj?.lecturers?.[0]?.name?.jp}`;
+
+          if (this.idsInTimetable.includes(id)) {
+            this.setInitialData(dataObj);
+            this.$set(this.courseDatas, id, dataObj);
+            return true;
+          }
+          return false;
+        });
     },
-    curLang() {
-      return this.$i18n.locale;
+    setInitialData(dataObj: CourseInfo) {
+      const times = dataObj?.schedule?.times?.jp;
+      const id = `${dataObj?.title?.name?.jp} ${dataObj?.lecturers?.[0]?.name?.jp}`;
+      const pattern = /([月火水木金土日])曜日([１２３４５６７1234567])時限/;
+      let [day, time]: number[] = [100, 100];
+      const week = ['月', '火', '水', '木', '金', '土', '日'];
+
+      if (typeof times === 'string') {
+        times.split(',').forEach((str) => {
+          const arr = pattern.exec(str);
+
+          if (arr) {
+            const toHalfWidth = String.fromCharCode(arr[2].charCodeAt(0) - 0xFEE0);
+            day = week.indexOf(arr[1]);
+            time = Number(toHalfWidth);
+          }
+        });
+      }
+      this.sortedIDs.push([id, [day, time]]);
     },
   },
 });
@@ -235,7 +167,7 @@ export default Vue.extend({
 <i18n>
 {
   "en": {
-    "days":  ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Others"]
+    "days": ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Others"]
   },
   "jp": {
     "days": ["月", "火", "水", "木", "金", "土", "その他"]
