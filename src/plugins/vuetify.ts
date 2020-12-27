462e7bf84337566ef595517b6067d5f58775cc00
OR-2000 <71973242+OR-2000@users.noreply.github.com>
2020-12-27 14:57:54 +0900

    refactor: change some color settings

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -6,20 +6,25 @@ Vue.use(Vuetify);
 
 export default new Vuetify({
   theme: {
+    options: {
+      customProperties: true,
+    },
     dark: false,
     themes: {
       light: {
+        cover: '#f7f7f7',
         white: colors.shades.white,
-        black: '#5D6B79',
-        primary: '#2C3E50',
-        secondary: '#405061',
+        black: '#757575',
+        primary: '#424242',
+        secondary: '#BDBDBD',
         accent: colors.shades.white,
       },
       dark: {
+        cover: '#2a2a2a',
         white: '#121212',
         black: '#b2b2b2',
-        primary: colors.grey.darken4,
-        secondary: colors.grey.darken3,
+        primary: '#E0E0E0',
+        secondary: '#757575',
         accent: '#b2b2b2',
       },
     },
