fe30b34209224692111c2d38b6d05e508fdd0d90
OR-2000 <71973242+OR-2000@users.noreply.github.com>
2020-12-27 14:59:18 +0900

    refactor: remove unnecessary declaration

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,32 +1,36 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import axios from 'axios';
-import router from '@/router';
 import { CourseInfo } from '@/assets/CourseInfo';
 
 Vue.use(Vuex);
 
 const courses: CourseInfo[] = [];
 
-const setTimetable = (timetable: string) => {
-  localStorage.setItem('idsInTimetable', timetable);
+const setTimetable = (ids: string[]) => {
+  localStorage.setItem('idsInTimetable', JSON.stringify(ids));
 };
+
+const idsType: string[] = [];
 
 export default new Vuex.Store({
   state: {
     isLoading: false,
     courseDatas: courses,
     length: 0,
-    idsInTimetable: [''],
+    idsInTimetable: idsType,
   },
   mutations: {
     async fetchData(state, search: string) {
       const url = new URL('http://54.248.214.173:8000/search');
       url.search = search;
+
       state.isLoading = true;
+
       try {
         const response = await axios.get(url.href);
-        state.courseDatas = JSON.parse(JSON.stringify(response.data)).Hits;
+        const datas = JSON.parse(JSON.stringify(response.data)).Hits;
+        state.courseDatas = datas;
         if (state.courseDatas === null) {
           throw new Error();
         } else {
@@ -34,26 +38,29 @@ export default new Vuex.Store({
         }
       } catch (e) {
         state.courseDatas = courses;
-        router.push('/404');
       } finally {
         state.isLoading = false;
       }
     },
     getIdsFromLocalStorage(state) {
       const idsInlocalStorage = localStorage.getItem('idsInTimetable');
+
       if (typeof idsInlocalStorage === 'string') {
         state.idsInTimetable = JSON.parse(idsInlocalStorage);
       } else {
-        setTimetable(JSON.stringify(state.idsInTimetable));
+        setTimetable(state.idsInTimetable);
       }
     },
     addToTimetable(state, payload: string) {
       state.idsInTimetable = [...state.idsInTimetable, payload];
-      setTimetable(JSON.stringify(state.idsInTimetable));
+
+      setTimetable(state.idsInTimetable);
     },
     removeFromTimetable(state, payload: string) {
-      state.idsInTimetable = state.idsInTimetable.filter((id) => id !== payload);
-      setTimetable(JSON.stringify(state.idsInTimetable));
+      state.idsInTimetable = state.idsInTimetable
+        .filter((str: string) => str !== payload);
+
+      setTimetable(state.idsInTimetable);
     },
   },
   actions: {
