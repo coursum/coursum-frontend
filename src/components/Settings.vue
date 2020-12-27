79034900aff4c32eb18f105514a53ede62ecd57a
OR-2000 <71973242+OR-2000@users.noreply.github.com>
2020-12-27 14:46:25 +0900

    refactor: move Settings.vue to components

---
+++ src/components/Settings.vue
@@ -0,0 +1,95 @@
+<template>
+  <v-card
+    width="350px"
+    class="pa-6"
+    color="white"
+  >
+    <v-row>
+      <v-col class="d-flex align-center">
+        {{ $t("darkMode") }}
+      </v-col>
+      <v-col>
+        <v-switch
+          v-model="themeSwitch"
+          @click.native.stop
+        />
+      </v-col>
+    </v-row>
+    <v-divider />
+    <v-row>
+      <v-col class="d-flex align-center">
+        {{ $t("lang") }}
+      </v-col>
+      <v-col>
+        <v-chip-group
+          v-model="language"
+          mandatory
+        >
+          <v-chip
+            v-for="([locale, localeName], idx) in Object.entries(languages)"
+            :key="idx"
+            outlined
+            @click.stop="setLang(locale)"
+          >
+            {{ localeName }}
+          </v-chip>
+        </v-chip-group>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+
+  name: 'Settings',
+
+  data() {
+    return {
+      languages: { jp: '日本語', en: 'English' },
+      language: 0,
+      themeSwitch: false,
+    };
+  },
+  watch: {
+    themeSwitch() {
+      this.$vuetify.theme.dark = this.themeSwitch;
+      this.setDarkModeState();
+    },
+  },
+  created() {
+    if (this.$root.$i18n.locale === 'jp') {
+      this.language = 0;
+    } else {
+      this.language = 1;
+    }
+
+    this.themeSwitch = this.$vuetify.theme.dark;
+  },
+  methods: {
+    setLang(locale: string) {
+      this.$root.$i18n.locale = locale;
+    },
+    setDarkModeState() {
+      localStorage.setItem('darkModeState', JSON.stringify(this.$vuetify.theme.dark));
+    },
+  },
+});
+</script>
+
+<i18n>
+{
+  "en": {
+    "darkMode": "Dark mode",
+    "lang": "Language",
+    "languages": ["jp", "en"]
+  },
+  "jp": {
+    "darkMode": "ダークモード",
+    "lang": "言語",
+    "languages": ["日本語", "英語"]
+  }
+}
+</i18n>
