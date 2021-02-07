46570cc27b7f8b676060e7bdf16e2b65a2d50453
OR-2000 <71973242+OR-2000@users.noreply.github.com>
2021-02-08 00:06:11 +0900

    Add vue composition api

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,20 @@
-import Vue from 'vue';
-import App from './App.vue';
 import './registerServiceWorker';
+import Vue from 'vue';
+import VueCompositionApi from '@vue/composition-api';
+import App from './App.vue';
+import i18n from './i18n';
 import router from './router';
 import store from './store';
 import vuetify from './plugins/vuetify';
-import i18n from './i18n';
 
 Vue.config.productionTip = false;
 
+Vue.use(VueCompositionApi);
+
 new Vue({
+  i18n,
   router,
   store,
   vuetify,
-  i18n,
   render: (h) => h(App),
 }).$mount('#app');
