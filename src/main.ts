b8d3d90ef938ba2a3dbe15a5dc20d4d0fe62c0d2
OR-2000 <71973242+OR-2000@users.noreply.github.com>
2021-05-27 10:05:47 +0900

    build: configuration for @vue/composition-api and element-ui

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,22 @@
-import Vue from 'vue';
-import App from './App.vue';
 import './registerServiceWorker';
+import Element from 'element-ui';
+import lang from 'element-ui/lib/locale/lang/en';
+import locale from 'element-ui/lib/locale';
+import Vue from 'vue';
+import VueCompositionApi from '@vue/composition-api';
 import router from './router';
 import store from './store';
-import vuetify from './plugins/vuetify';
 import i18n from './i18n';
+import App from './App.vue';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
+Vue.use(VueCompositionApi);
+
+// configure language
+// https://element.eleme.io/#/en-US/component/i18n#internationalization
+locale.use(lang);
+Vue.use(Element);
 
 new Vue({
   router,
