f9c09b5b0cd14ac5586e3308c1ab237f93e6baec
LuckyWind_sck <luckywindsck@gmail.com>
2021-05-29 15:54:00 +0900

    fix: show course time correctly

--- src/components/course/data/d_schedule_times.vue
+++ src/components/course/data/d_schedule_times.vue
@@ -28,24 +28,26 @@ export default defineComponent({
   setup: (props) => {
     const displays = computed((): string[] | undefined => {
       const times = props.times.jp;
-      if (times) {
-        const pattern = /([月火水木金土日])曜日([１２３４５６７1234567])時限/;
-        const schedule: string[] = times.split(',').map((str: string) => {
-          const arr = pattern.exec(str);
-          let [day, time]: [string, number] = ['', -1];
 
-          if (arr) {
-            const toHalfWidth = String.fromCharCode(arr[2].charCodeAt(0) - 0xFEE0);
-            [, day] = arr;
-            time = Number(toHalfWidth);
-          }
+      if (!times) return undefined;
 
-          return day + time;
-        });
+      const schedule: string[] = times.split(' , ');
 
-        return schedule;
-      }
-      return undefined;
+      // const pattern = /([月火水木金土日])曜日([１２３４５６７1234567])時限/;
+      // const schedule: string[] = times.split(',').map((str: string) => {
+      //   const arr = pattern.exec(str);
+      //   let [day, time]: [string, number] = ['', -1];
+
+      //   if (arr) {
+      //     const toHalfWidth = String.fromCharCode(arr[2].charCodeAt(0) - 0xFEE0);
+      //     [, day] = arr;
+      //     time = Number(toHalfWidth);
+      //   }
+
+      //   return day + time;
+      // });
+
+      return schedule;
     });
 
     return {
