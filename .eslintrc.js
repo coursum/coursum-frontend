f7f5b2f8f49dce7094013ded5055021ebe88cce3
LuckyWind_sck <luckywindsck@gmail.com>
2021-05-30 01:05:28 +0900

    build: config linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,8 @@ module.exports = {
   },
   extends: [
     'plugin:vue/recommended',
+    'plugin:vue/strongly-recommended',
+    'plugin:vue/recommended',
     '@vue/airbnb',
     '@vue/typescript/recommended',
   ],
@@ -14,6 +16,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+    'vue/max-attributes-per-line': 'off',
+
     'import/order': [
       'error',
       {
