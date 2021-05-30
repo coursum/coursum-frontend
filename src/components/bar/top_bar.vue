13a7da5a76f12ce6ad382682b465dd367dbf29e1
LuckyWind_sck <luckywindsck@gmail.com>
2021-05-30 14:06:34 +0900

    refactor: use v-text-field for search bar & split setup
    1. convert v-combobox to v-text-field since we are not utilizing
    combobox api
    
    2. split setup into composition functions

--- src/components/bar/top_bar.vue
+++ src/components/bar/top_bar.vue
@@ -2,17 +2,13 @@
   <v-app-bar app color="primary">
     <v-app-bar-nav-icon @click.stop="toggleSideBar" />
 
-    <!-- <v-btn icon @click="goBack">
-      <v-icon>mdi-arrow-left</v-icon>
-    </v-btn> -->
-
-    <span v-if="isMdAndUp" class="coursum-logo" @click="goResult">
+    <span v-if="isMdAndUp" class="coursum-title" @click="goResult">
       Coursum
     </span>
 
     <v-spacer />
 
-    <search-input />
+    <search-input :style="searchBarWidth" class="mx-auto" />
     <span class="mx-1">
       <keep-alive>
         <advanced-inputs />
@@ -24,12 +20,52 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent } from '@vue/composition-api';
+import { SetupContext, computed, defineComponent } from '@vue/composition-api';
 
 import tool from '@/api/build_query';
 import request from '@/api/request';
 import AdvancedInputs from '@/components/search/advanced_inputs.vue';
 import SearchInput from '@/components/search/search_input.vue';
+
+const useSideBar = (context: SetupContext) => {
+  const { $store } = context.root;
+
+  const toggleSideBar = () => {
+    $store.commit('sidebar/toggle', !$store.state.sidebar.isVisible);
+  };
+
+  return { toggleSideBar };
+};
+
+const useCoursumTitle = (context: SetupContext) => {
+  const { $vuetify } = context.root;
+
+  const isMdAndUp = computed(() => $vuetify.breakpoint.mdAndUp);
+
+  // TODO: Dont fetch course in index page, fetch only after search.
+  const goResult = async () => {
+    await request.fetchAndStoreCourses('search?');
+
+    tool.goToResultPage('/');
+  };
+
+  return { isMdAndUp, goResult };
+};
+
+const useSearchBar = (context: SetupContext) => {
+  const { $vuetify } = context.root;
+
+  const searchBarWidth = computed(() => {
+    let width = '40%';
+
+    if ($vuetify.breakpoint.mdAndDown) width = '60%';
+    if ($vuetify.breakpoint.xs) width = '80%';
+
+    return { width };
+  });
+
+  return { searchBarWidth };
+};
 
 export default defineComponent({
   name: 'TopBar',
@@ -37,40 +73,16 @@ export default defineComponent({
     AdvancedInputs,
     SearchInput,
   },
-  setup: (_, { root: { $store, $vuetify } }) => {
-    // const goBack = () => {
-    //   if (window.history.length > 1) {
-    //     $router.go(-1);
-    //   } else {
-    //     $router.push('/');
-    //   }
-    // };
-
-    const isMdAndUp = computed(() => $vuetify.breakpoint.mdAndUp);
-
-    // TODO: Dont fetch course in index page, fetch only after search.
-    const goResult = async () => {
-      await request.fetchAndStoreCourses('search?');
-
-      tool.goToResultPage('/');
-    };
-
-    const toggleSideBar = () => {
-      $store.commit('sidebar/toggle', !$store.state.sidebar.isVisible);
-    };
-
-    return {
-      // goBack,
-      isMdAndUp,
-      goResult,
-      toggleSideBar,
-    };
-  },
+  setup: (_, context) => ({
+    ...useSideBar(context),
+    ...useCoursumTitle(context),
+    ...useSearchBar(context),
+  }),
 });
 </script>
 
 <style scoped>
-.coursum-logo {
+.coursum-title {
   font-size: 1.1rem;
   cursor: pointer;
 }
--- src/components/search/search_input.vue
+++ src/components/search/search_input.vue
@@ -1,85 +1,50 @@
 <template>
-  <div
-    id="search"
-    :style="searchBarWidth"
-    class="mx-auto"
-  >
-    <v-combobox
-      :filled="true"
-      :dense="true"
-      rounded
-      class="rounded-lg"
-      prepend-inner-icon="mdi-magnify"
-      type="text"
-      hide-details
-      :search-input.sync="input"
-      :value="input"
-      @keydown.enter="goResult"
-    />
-  </div>
+  <v-text-field
+    v-model="input"
+
+    class="rounded-lg"
+
+    dense
+    filled
+    hide-details
+    rounded
+    prepend-inner-icon="mdi-magnify"
+    clearable
+
+    @keydown.enter="search"
+  />
 </template>
 
 <script lang="ts">
-import {
-  computed, defineComponent, reactive, toRefs,
-} from '@vue/composition-api';
+import { SetupContext, defineComponent, ref } from '@vue/composition-api';
 
 import tool from '@/api/build_query';
 import request from '@/api/request';
 
+const useSearch = (context: SetupContext) => {
+  const { $store } = context.root;
+
+  const input = ref('');
+
+  const search = async () => {
+    const searchQuery = tool.buildQuery({
+      builder: input.value,
+    });
+
+    await request.fetchAndStoreCourses(searchQuery);
+
+    $store.commit('setSearchInput', input.value);
+
+    tool.goToResultPage(`/course/${searchQuery}`);
+  };
+
+  return { input, search };
+};
+
 export default defineComponent({
   name: 'SearchInput',
-  setup: (_, context) => {
-    const state = reactive({
-      input: '',
-    });
-
-    const searchBarWidth = computed((): object => {
-      let widthRate;
-      const { breakpoint } = context.root.$vuetify;
-
-      if (breakpoint.xs) {
-        widthRate = 80;
-      } else if (breakpoint.mdAndDown) {
-        widthRate = 60;
-      } else {
-        widthRate = 40;
-      }
-
-      return { width: `${widthRate}%` };
-    });
-
-    const flat = computed((): boolean => context.root.$route.path !== '/' && !context.root.$route.params.query);
-
-    const goResult = async () => {
-      const searchQuery = tool.buildQuery({
-        builder: state.input,
-      });
-
-      await request.fetchAndStoreCourses(searchQuery);
-
-      context.root.$store.commit('setSearchInput', state.input);
-
-      tool.goToResultPage(`/course/${searchQuery}`);
-    };
-
-    return {
-      searchBarWidth,
-      flat,
-      goResult,
-      ...toRefs(state),
-    };
-  },
+  setup: (_, context) => ({
+    ...useSearch(context),
+  }),
 });
 </script>
-
-<style>
-#search .v-text-field input {
-  padding: 0px;
-  line-height: 30px !important;
-}
-
-#search .v-icon {
-  color: #bdbdbd;
-}
-</style>
