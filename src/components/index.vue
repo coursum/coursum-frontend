53a1de898b582d192269590ba179896de618acc2
LuckyWind_sck <luckywindsck@gmail.com>
2021-07-08 17:42:40 +0900

    feat: show all courses

--- src/components/index.vue
+++ src/components/index.vue
@@ -1,3 +1,18 @@
 <template>
   <div>Index</div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from '@vue/composition-api';
+
+import useRouter from '@/util/use-router';
+
+export default defineComponent({
+  name: 'Index',
+  setup: (_, context) => {
+    const { routerPush } = useRouter(context.root.$router);
+
+    routerPush('/search?all');
+  },
+});
+</script>
